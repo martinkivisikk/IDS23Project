eac483704b7b37ecd12863c4d097015efde51808
martinkivisikk <martinkivisikk@gmail.com>
2023-11-23 13:24:39 +0200

    Trained a Naive Bayes model, added it to the web app.

--- app.py
+++ app.py
@@ -1,25 +1,75 @@
-from flask import Flask, render_template, request
 import joblib
+from flask import Flask, render_template, request
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
 
 app = Flask(__name__)
 
-# Load your trained model
-#spam_classifier_model = joblib.load('path/to/your/model.pkl')
+# Define the available models and their paths
+model_paths = {
+    'NaiveBayes': 'models/NaiveBayes.pkl',
+    # Add more models as needed
+}
+
+# Load your trained model and vectorizer
+models = {}
+for model_name, model_path in model_paths.items():
+    try:
+        models[model_name] = joblib.load(model_path)
+    except Exception as e:
+        app.logger.error(f"Error loading model '{model_name}': {str(e)}")
+
+vectorizer_path = 'models/vectorizer.pkl'
+try:
+    vectorizer = joblib.load(vectorizer_path)
+except Exception as e:
+    app.logger.error(f"Error loading vectorizer: {str(e)}")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        email_text = request.form['email_text']
+    try:
+        if request.method == 'POST':
+            email_text = request.form['email_text']
+            selected_model = request.form['model']
 
-        # Perform any necessary preprocessing on the input text (similar to what you did during training)
+            # Validate and sanitize user input
+            if not email_text:
+                raise ValueError("Email text is required.")
 
-        # Make prediction
-        #prediction = spam_classifier_model.predict([email_text])
-        prediction = [0,1]
-        # Display the prediction
-        return render_template('index.html', prediction=prediction[0], email_text=email_text)
+            # Perform any necessary preprocessing on the input text
+            cleaned_text = ' '.join(word for word in email_text.split())
+            tokenized_text = cleaned_text.split()
+            lowercased_text = [word.lower() for word in tokenized_text]
+            stop_words = set(stopwords.words('english'))
+            filtered_text = [word for word in lowercased_text if word not in stop_words]
+            lemmatizer = WordNetLemmatizer()
+            preprocessed_text = ' '.join([lemmatizer.lemmatize(word) for word in filtered_text])
 
-    return render_template('index.html', prediction=None, email_text=None)
+            # Vectorize the preprocessed text
+            input_vector = vectorizer.transform([preprocessed_text])
+
+            # Make prediction and get probability
+            if selected_model not in models:
+                raise ValueError(f"Selected model '{selected_model}' is not available.")
+
+            prediction = models[selected_model].predict(input_vector)
+            probability = models[selected_model].predict_proba(input_vector)[:, 1]  # Assuming binary classification
+
+            # Convert prediction to "Spam" or "Not Spam"
+            prediction_label = "Spam" if prediction[0] == 1 else "Not spam"
+            probability = 1 - probability[0] if prediction[0] == 0 else probability[0]
+
+            # Display the prediction and probability
+            return render_template('index.html', prediction=prediction_label, probability=probability,
+                                   email_text=email_text, selected_model=selected_model, model_names=model_paths.keys())
+
+    except Exception as e:
+        app.logger.error(f"Error during prediction: {str(e)}")
+
+    return render_template('index.html', prediction=None, probability=None, email_text="", selected_model=None,
+                           model_names=model_paths.keys())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/model.py
@@ -0,0 +1,75 @@
+import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# May need to download some packages from NLTK, such as stopwords, wordnet.
+# Uncomment the following lines to download the packages.
+# Download stopwords and wordnet
+import nltk
+nltk.download("stopwords")
+nltk.download("wordnet")
+
+"""
+    Text Cleaning: Remove any unnecessary characters, symbols, or HTML tags.
+
+    Tokenization: Split the text into individual words or tokens.
+
+    Lowercasing: Convert all text to lowercase to ensure consistency.
+
+    Stopword Removal: Remove common words that don't carry much information (e.g., "and", "the", "is").
+
+    Stemming or Lemmatization: Reduce words to their base or root form.
+
+    Vectorization: Convert the text data into a numerical format. This can be done using techniques like Bag-of-Words or TF-IDF.
+"""
+# Load your dataset
+data = pd.read_csv('../data/combined_data.csv')
+
+# Step 1: Text Cleaning
+data['cleaned_text'] = data['text'].apply(lambda x: ' '.join(word for word in x.split()))
+
+# Step 2: Tokenization
+data['tokenized_text'] = data['cleaned_text'].apply(lambda x: x.split())
+
+# Step 3: Lowercasing
+data['tokenized_text'] = data['tokenized_text'].apply(lambda x: [word.lower() for word in x])
+
+# Step 4: Stopword Removal (you may need to download the stopword list)
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
+data['tokenized_text'] = data['tokenized_text'].apply(lambda x: [word for word in x if word not in stop_words])
+
+# Step 5: Stemming or Lemmatization (you may need to download a lemmatizer)
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+data['tokenized_text'] = data['tokenized_text'].apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
+
+# Step 6: Vectorization (choose either CountVectorizer or TfidfVectorizer)
+vectorizer = TfidfVectorizer(max_features=5000)  # You can adjust max_features based on your dataset size
+X = vectorizer.fit_transform(data['tokenized_text'].apply(lambda x: ' '.join(x)))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, data['label'], test_size=0.2, random_state=42)
+
+# Train a model (using Naive Bayes as an example)
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# Save the model and vectorizer to pkl files
+joblib.dump(model, 'NaiveBayes.pkl')
+joblib.dump(vectorizer, 'vectorizer.pkl')
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+
+# You can also print other metrics like classification report
+print(classification_report(y_test, y_pred))
